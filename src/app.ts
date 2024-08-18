34b412944d9e48fcaf496d592823709de7c394b9
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-08-18 16:43:19 -0400

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import compression from 'compression';
-import cors from 'cors';
 import express from 'express';
 import { AuthRouter } from './auth/auth.router';
 import { AppDataSource } from './database';
@@ -9,13 +8,14 @@ import { DestinationRouter } from './domains/destinations/destination.router';
 import { FlightRouter } from './domains/flights/flight.router';
 import { PassengerRouter } from './domains/passengers/passenger.router';
 import { UserRouter } from './domains/users/user.router';
+import { cors } from './middlewares/cors';
 import { debug } from './middlewares/debug';
 
 export namespace App {
   export const server = express();
 
   server.use(compression());
-  server.use(cors({ allowedHeaders: 'Access-Control-Allow-Origin' }));
+  server.use(cors);
   server.use(express.json());
 
   server.use(debug);
